777627e91c9d98add6e2efe5bf3a6c3b8fffaafb
ksenija0308 <x.koroljova@gmail.com>
2025-07-05 10:51:27 +0200

    ✨ Add lion reward image + popup mint confirmation

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    },
+      contextIsolation: false, // allows ipcRenderer and require in HTML
+    }
   });
 
   win.loadFile('index.html');
@@ -15,3 +18,18 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 });
+
+// Listen for popup request from renderer and bring to front
+ipcMain.on('bring-to-front', () => {
+  if (win) {
+    win.show();
+    win.setAlwaysOnTop(true); // Force window to top
+    win.focus();
+
+    // Remove always-on-top after a few seconds
+    setTimeout(() => {
+      win.setAlwaysOnTop(false);
+    }, 3000);
+  }
+});
+
